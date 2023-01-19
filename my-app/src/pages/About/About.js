7c6e1dc8b665f53045219466352b01499bcb59bf
KayDaaH <thomas36.rolland@gmail.com>
2023-01-19 15:16:47 +0100

    Routes créées, début du projet

---
+++ my-app/src/pages/About/About.js
@@ -0,0 +1,7 @@
+import styles from "./About.module.scss";
+
+function About() {
+  return <div className={styles.main}>Page about</div>;
+}
+
+export default About;
---
+++ my-app/src/pages/Home/Home.js
@@ -0,0 +1,7 @@
+import styles from "./Home.module.scss";
+
+function Home() {
+  return <div className={styles.main}>Pade HOME</div>;
+}
+
+export default Home;

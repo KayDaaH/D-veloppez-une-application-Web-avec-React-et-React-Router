880e76671f50158fc4dfd6ee50d9a93960641a67
KayDaaH <thomas36.rolland@gmail.com>
2023-01-23 04:07:50 +0100

    Dernière sauvegarde

--- my-app/src/components/Location/Location.js
+++ my-app/src/components/Location/Location.js
@@ -2,7 +2,6 @@ import React from "react";
 import styles from "./Location.module.scss";
 
 const Location = ({ titre }) => {
-  console.log(titre);
   return (
     <div className={styles.main}>
       <p>{titre}</p>

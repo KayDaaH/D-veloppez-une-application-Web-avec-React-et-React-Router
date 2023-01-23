e16f5e8cf47ea6622cb8993e90a5ba84c681a51d
KayDaaH <thomas36.rolland@gmail.com>
2023-01-23 23:30:18 +0100

    Ajout de la page a propos

---
+++ my-app/src/components/AboutMenu/AboutMenu.js
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+import styles from "./AboutMenu.module.scss";
+
+const AboutMenu = ({ title, text }) => {
+  const [dropdownIsOpen, setdropdownIsOpen] = useState(false);
+
+  function openDropdown() {
+    setdropdownIsOpen(!dropdownIsOpen);
+  }
+
+  return (
+    <div className={styles.main}>
+      <div className={styles.title}>
+        <p>{title}</p>
+        <div
+          className={
+            dropdownIsOpen
+              ? `${styles.icon} ${styles.active}`
+              : `${styles.icon}`
+          }
+          onClick={() => openDropdown()}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6 test"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M19.5 8.25l-7.5 7.5-7.5-7.5"
+            />
+          </svg>
+        </div>
+      </div>
+      <div
+        className={
+          dropdownIsOpen ? `${styles.text} ${styles.active}` : `${styles.text}`
+        }
+      >
+        {text}
+      </div>
+    </div>
+  );
+};
+
+export default AboutMenu;
--- my-app/src/pages/About/About.js
+++ my-app/src/pages/About/About.js
@@ -1,11 +1,38 @@
+// import Footer from "../../components/Footer/Footer";
+import AboutMenu from "../../components/AboutMenu/AboutMenu";
 import Footer from "../../components/Footer/Footer";
 import styles from "./About.module.scss";
 
 function About() {
   return (
     <div className={styles.main}>
-      Page about
+      <div className={styles.container}>
+        <div className={styles.bloc_image}>
+          <div className={styles.darken}></div>
+        </div>
+        <div className={styles.menus_container}>
+          <AboutMenu
+            title="Fiabilité"
+            text="Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes."
+          />
+          <AboutMenu
+            title="Respect"
+            text="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme."
+          />
+          <AboutMenu
+            title="Service"
+            text="Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question."
+          />
+          <AboutMenu
+            title="Sécurité"
+            text="La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes."
+          />
+        </div>
+      </div>
       <Footer />
+
+      {/* <div className={styles.footer}>
+      </div> */}
     </div>
   );
 }

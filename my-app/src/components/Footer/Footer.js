467512d34936e89f31ecf1a8d589c4103e415ce2
KayDaaH <thomas36.rolland@gmail.com>
2023-01-23 01:59:57 +0100

    Correction des détails des images

--- my-app/src/components/Footer/Footer.js
+++ my-app/src/components/Footer/Footer.js
@@ -4,9 +4,9 @@ import logoblack from "./logoblack.png";
 function Footer() {
   return (
     <header className={styles.main}>
-      <div className={styles.logo}>
-        <img className={styles.logoblack} src={logoblack} alt="logo" />
-      </div>
+      {/* <img className={styles.logoblack} src={logoblack} alt="logo" /> */}
+      <div className={styles.logoblack}></div>
+
       <div className={styles.mention}>© 2020 Kasa. All rights reserved</div>
     </header>
   );
--- my-app/src/pages/Home/Home.js
+++ my-app/src/pages/Home/Home.js
@@ -1,13 +1,14 @@
 import Footer from "../../components/Footer/Footer";
 import styles from "./Home.module.scss";
-import image from "./image.jpg";
+// import image from "./image.jpg";
 
 function Home() {
   return (
     <div className={styles.main}>
       <div className={styles.container}>
         <div className={styles.bloc_image}>
-          <img className={styles.landscape} src={image} alt="landscape" />
+          {/* <img className={styles.landscape} src={image} alt="landscape" /> */}
+          <div className={styles.darken}></div>
           <p className={styles.title}>Chez vous, partout et ailleurs</p>
         </div>
         <div className={styles.blocs_container}></div>

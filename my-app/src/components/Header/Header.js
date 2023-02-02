2df01daad279537ff40be4a6ac8a8ec26f20f829
KayDaaH <thomas36.rolland@gmail.com>
2023-02-02 09:36:50 +0100

    liens navbar soulignés sur la bonne page

--- my-app/src/components/Header/Header.js
+++ my-app/src/components/Header/Header.js
@@ -1,8 +1,10 @@
 import styles from "./Header.module.scss";
 import logo from "./logo.jpg";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 function Header() {
+  let { pathname } = useLocation();
+
   return (
     <header className={styles.main}>
       <div className={styles.container}>
@@ -11,14 +13,25 @@ function Header() {
         </div>
 
         <nav className={styles.nav}>
-          <ul className={styles.list}>
-            <li className={styles.link}>
-              <Link to="/">Accueil</Link>
-            </li>
-            <li className={styles.link}>
-              <Link to="/about">A Propos</Link>
-            </li>
-          </ul>
+          {pathname === "/" ? (
+            <ul className={styles.list}>
+              <li className={`${styles.link} ${styles.active}`}>
+                <Link to="/">Accueil</Link>
+              </li>
+              <li className={styles.link}>
+                <Link to="/about">A Propos</Link>
+              </li>
+            </ul>
+          ) : (
+            <ul className={styles.list}>
+              <li className={styles.link}>
+                <Link to="/">Accueil</Link>
+              </li>
+              <li className={`${styles.link} ${styles.active}`}>
+                <Link to="/about">A Propos</Link>
+              </li>
+            </ul>
+          )}
         </nav>
       </div>
     </header>
--- my-app/src/components/Stars/Stars.js
+++ my-app/src/components/Stars/Stars.js
@@ -7,7 +7,7 @@ const Stars = ({ rating }) => {
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 24 24"
       fill="currentColor"
-      class="w-6 h-6"
+      className="w-6 h-6"
     >
       <path
         fill-rule="evenodd"
